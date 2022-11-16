53f002d705f5ffc8f4b67e41a9243138a3977657
pib0t <tpoehlsen@gmail.com>
2022-11-16 08:42:08 +0100

    Update strato.js

--- _data/sites/strato.js
+++ _data/sites/strato.js
@@ -1,8 +1,16 @@
 module.exports = {
-	name: "Strato", // optional, falls back to object key
-	description: "The Strato sites that get tested",
+	name: "strato", // optional, falls back to object key
+	description: "strato web sites",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
 	},
 	urls: [
 		"https://www.strato.de/",
